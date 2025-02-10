c6b4fbbd02cda10165cf0ad2a80a7d9036164066
Ashok Battala <0511ashokkumar@gmail.com>
2025-02-10 20:46:24 +0530

    added EqualsAndHashCode interview concept

---
+++ InterviewQuestions/src/com/learning/java/interview/EqualsAndHashCodeDemo.java
@@ -0,0 +1,58 @@
+package com.learning.java.interview;
+
+import java.util.Objects;
+
+public class EqualsAndHashCodeDemo {
+
+    public static void main(String[] args) {
+        //String name = "ashok";
+        // String secondName = "ashok"; //(address  of name)
+
+
+        //String name = new String("ashok");
+        //String secondName = new String("ashok");
+        //String thirdName = "ashok";
+
+        //check the reference (==)
+        // System.out.println(name == secondName);
+
+        //checks the data
+        // System.out.println(name.equals(secondName));
+
+        //creating two Students
+        Student ashok = new Student(1, "ashok");
+        Student afzal = new Student(2, "ashok");
+
+        //checking equals
+//        System.out.println(ashok.equals(afzal));
+//        System.out.println(ashok == afzal);
+
+        //checking hashcode
+        System.out.println("ashok hashcode: " + ashok.hashCode());
+        System.out.println("afzal hashcode: " + afzal.hashCode());
+    }
+}
+class Student extends Object {
+    private int id;
+    private String name;
+
+    public Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return id == student.id && Objects.equals(name, student.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+}
+
+
